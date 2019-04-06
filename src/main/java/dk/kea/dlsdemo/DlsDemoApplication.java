cbc3f14fbbdb3a5e50f75fd85a113a9469d2113e
alex-prog <coag@kea.dk>
2019-04-06 14:17:38 +0200

    Initial commit

---
+++ src/main/java/dk/kea/dlsdemo/DlsDemoApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.dlsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DlsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/dlsdemo/DlsDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package dk.kea.dlsdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DlsDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
